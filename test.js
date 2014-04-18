84b50e3e9137aa5aab9295755aab3dd7d93e1e4f
Sindre Sorhus <sindresorhus@gmail.com>
2014-04-18 10:31:02 +0200

    bump gzip-size

--- test.js
+++ test.js
@@ -3,10 +3,10 @@ var assert = require('assert');
 var chalk = require('chalk');
 var maxmin = require('./index');
 
-var max = 'function smoothRangeRandom(min,max){var num=Math.floor(Math.random()*(max-min+1)+min);return this.prev=num===this.prev?++num:num};';
-var min = '(function(b,c){var a=Math.floor(Math.random()*(c-b+1)+b);return this.a=a===this.a?++a:a})()';
+var max = 'function smoothRangeRandom(min,max){var num=Math.floor(Math.random()*(max-min+1)+min);return this.prev=num===this.prev?++num:num};function smoothRangeRandom(min,max){var num=Math.floor(Math.random()*(max-min+1)+min);return this.prev=num===this.prev?++num:num};function smoothRangeRandom(min,max){var num=Math.floor(Math.random()*(max-min+1)+min);return this.prev=num===this.prev?++num:num};';
+var min = 'function smoothRangeRandom(b,c){var a=Math.floor(Math.random()*(c-b+1)+b);return this.prev=a===this.prev?++a:a}function smoothRangeRandom(b,c){var a=Math.floor(Math.random()*(c-b+1)+b);return this.prev=a===this.prev?++a:a}function smoothRangeRandom(b,c){var a=Math.floor(Math.random()*(c-b+1)+b);return this.prev=a===this.prev?++a:a};';
 
 it('should generate correct output', function () {
-	assert.strictEqual(chalk.stripColor(maxmin(max, min)), '130 B → 91 B');
-	assert.strictEqual(chalk.stripColor(maxmin(max, min, true)), '130 B → 91 B → 53 B (gzip)');
+	assert.strictEqual(chalk.stripColor(maxmin(max, min)), '390 B → 334 B');
+	assert.strictEqual(chalk.stripColor(maxmin(max, min, true)), '390 B → 334 B → 120 B (gzip)');
 });
