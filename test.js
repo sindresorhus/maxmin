0f7bf4de3b1546ac11121430922dddd97b1a53fc
Sindre Sorhus <sindresorhus@gmail.com>
2014-07-10 03:28:21 +0200

    use `figures` module
    https://github.com/sindresorhus/figures

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strict';
 var assert = require('assert');
 var chalk = require('chalk');
-var maxmin = require('./index');
+var maxmin = require('./');
 
 var max = 'function smoothRangeRandom(min,max){var num=Math.floor(Math.random()*(max-min+1)+min);return this.prev=num===this.prev?++num:num};function smoothRangeRandom(min,max){var num=Math.floor(Math.random()*(max-min+1)+min);return this.prev=num===this.prev?++num:num};function smoothRangeRandom(min,max){var num=Math.floor(Math.random()*(max-min+1)+min);return this.prev=num===this.prev?++num:num};';
 var min = 'function smoothRangeRandom(b,c){var a=Math.floor(Math.random()*(c-b+1)+b);return this.prev=a===this.prev?++a:a}function smoothRangeRandom(b,c){var a=Math.floor(Math.random()*(c-b+1)+b);return this.prev=a===this.prev?++a:a}function smoothRangeRandom(b,c){var a=Math.floor(Math.random()*(c-b+1)+b);return this.prev=a===this.prev?++a:a};';
