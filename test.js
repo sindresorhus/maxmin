5ee77b4fac692418c44eb118c6b4cde7e50ef492
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-23 22:18:57 +0100

    bump dependencies

--- test.js
+++ test.js
@@ -5,20 +5,17 @@ import maxmin from './';
 const max = 'function smoothRangeRandom(min,max){var num=Math.floor(Math.random()*(max-min+1)+min);return this.prev=num===this.prev?++num:num};function smoothRangeRandom(min,max){var num=Math.floor(Math.random()*(max-min+1)+min);return this.prev=num===this.prev?++num:num};function smoothRangeRandom(min,max){var num=Math.floor(Math.random()*(max-min+1)+min);return this.prev=num===this.prev?++num:num};';
 const min = 'function smoothRangeRandom(b,c){var a=Math.floor(Math.random()*(c-b+1)+b);return this.prev=a===this.prev?++a:a}function smoothRangeRandom(b,c){var a=Math.floor(Math.random()*(c-b+1)+b);return this.prev=a===this.prev?++a:a}function smoothRangeRandom(b,c){var a=Math.floor(Math.random()*(c-b+1)+b);return this.prev=a===this.prev?++a:a};';
 
-test('generates correct output for strings', t => {
+test('strings', t => {
 	t.is(chalk.stripColor(maxmin(max, min)), '390 B → 334 B');
 	t.is(chalk.stripColor(maxmin(max, min, true)), '390 B → 334 B → 120 B (gzip)');
-	t.end();
 });
 
-test('generates correct output for buffers', t => {
+test('buffers', t => {
 	t.is(chalk.stripColor(maxmin(new Buffer(max), new Buffer(min))), '390 B → 334 B');
 	t.is(chalk.stripColor(maxmin(new Buffer(max), new Buffer(min), true)), '390 B → 334 B → 120 B (gzip)');
-	t.end();
 });
 
-test('generates correct output for integers', t => {
+test('integers', t => {
 	t.is(chalk.stripColor(maxmin(max.length, min.length)), '390 B → 334 B');
 	t.is(chalk.stripColor(maxmin(max.length, min.length, true)), '390 B → 334 B');
-	t.end();
 });
